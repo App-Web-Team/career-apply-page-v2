c5f7675ee65efb5cb63a5cf032d8e82926ff8545
md-ameen <mdameenbf@gmail.com>
2022-11-16 16:41:01 +0530

    form 3 fields corrected

--- sections/form-3.php
+++ sections/form-3.php
@@ -1,4 +1,3 @@
-
 <!-- <div class="form-3-move-up"> -->
 <div class="mt-5">
     <p class="details-heading experience-text text-capitalize">
@@ -8,41 +7,37 @@
         <div class="row f-row mt-5">
             <div class="col">
                 <input required type="text" />
-                <label>degree</label>
+                <label>designation</label>
             </div>
             <div class="col">
                 <input required type="text" />
-                <label>college</label>
+                <label>company name</label>
             </div>
         </div>
         <div class="row f-row my-5">
             <div class="col">
                 <input required type="text" />
-                <label>year <span class="text-lowercase">of</span> graduation</label>
+                <label>experience</label>
             </div>
             <div class="col">
                 <input required type="text" />
-                <label><span class="text-uppercase">cgpa</span> / percentage</label>
+                <label><span class="text-uppercase">ctc</span></label>
             </div>
         </div>
         <div class="row f-row">
             <button class="btn add-btn text-capitalize ml-3">+ add</button>
         </div>
         <div class="row f-row my-5">
-            <p class="please-text ml-3">Please list out all the certification throughout the years</p>
+            <p class="please-text ml-3">Please list out all the prof. skills you used throughout the years</p>
         </div>
         <div class="row f-row my-5">
             <div class="col">
                 <input required type="text" />
-                <label>certifications</label>
+                <label>skills</label>
             </div>
             <div class="col">
                 <input required type="text" />
-                <label>year</label>
-            </div>
-            <div class="col">
-                <input required type="text" />
-                <label>provided by</label>
+                <label>years <span class="text-lowercase">in practice</span></label>
             </div>
         </div>
         <div class="row f-row my-0 mb-3">
